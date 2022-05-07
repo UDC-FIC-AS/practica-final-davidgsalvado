48c4e1860713974e1713662838d784442d89d7f4
Sebastian Alfredo Castro Rampersad <s.castro.rampersad@udc.es>
2022-05-07 23:47:03 +0200

    Fixed UI

--- lib/emailservice/iu.ex
+++ lib/emailservice/iu.ex
@@ -2,6 +2,8 @@ defmodule Ui do
 
   use Agent
 
+  #Lanzar nodo cliente y ejecutar Ui.init_ui(:"dir@[IP]")
+
   def init_ui(dir_node) do
     conx = Client.init(dir_node)
     case conx do
@@ -12,35 +14,72 @@ defmodule Ui do
     end
   end
 
-  def send_req_user(dir_node, req) do
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  defp send_req_user(dir_node, req) do
     Client.send_request(dir_node, {:user_lbs, req}, true)
   end
 
-  def send_req_mess(dir_node, req) do
+  defp send_req_mess(dir_node, req) do
     Client.send_request(dir_node, {:message_lbs, req}, true)
   end
 
-  def prompt_1(dir_node) do
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  defp tuple_to_list({sender, message}) do
+    [sender | [message | []]]
+  end
+
+  defp get_message_to_send(l) do
+    [_ | t] = l
+    [_ | message] = t
+    Enum.join(message, " ")
+  end
+
+  defp display_aux(dir_node, text, type) do
+    Process.sleep(200) #Sleep para evitar problemas con en compilador interactivo.
+    Prompt.display(text)
+    case type do
+      1 -> prompt_1(dir_node)
+      2 -> prompt_2(dir_node)
+    end
+  end
+
+  defp print_help_aux() do
+    Prompt.display("register [username] [password]\nlogin [usernnme] [password]\nlogout\nlist_users\nsend [username] [message]\nnew_messages\nall_messages\ndelete_read_messages\n")
+  end
+
+  defp print_help(dir_node, type) do
+    print_help_aux()
+    case type do
+      1 -> prompt_1(dir_node)
+      2 -> prompt_2(dir_node)
+    end
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  defp prompt_1(dir_node) do
     cmd = Prompt.text(">")
-    splited_cmd = String.split(cmd, " ", trim: true)
-    if Enum.count(splited_cmd) == 0 do
+    splitted_cmd = String.split(cmd, " ", trim: true)
+    if Enum.count(splitted_cmd) == 0 do
       prompt_1(dir_node)
     else
-      [action | _] = splited_cmd
+      [action | _] = splitted_cmd
 
       case action do
         "register" ->
-          if Enum.count(splited_cmd) >= 3 do
-            Agent.update(:username, fn _ -> Enum.at(splited_cmd, 1) end)
-            send_req_user(dir_node, {:register, {Enum.at(splited_cmd, 1), Enum.at(splited_cmd, 2)}})
+          if Enum.count(splitted_cmd) >= 3 do
+            Agent.update(:username, fn _ -> Enum.at(splitted_cmd, 1) end)
+            send_req_user(dir_node, {:register, {Enum.at(splitted_cmd, 1), Enum.at(splitted_cmd, 2)}})
           else
             Prompt.display("incorrect format : register [username] [password]")
             prompt_1(dir_node)
           end
         "login" ->
-          if Enum.count(splited_cmd) >= 3 do
-            Agent.update(:username, fn _ -> Enum.at(splited_cmd, 1) end)
-            send_req_user(dir_node, {:login, {Enum.at(splited_cmd, 1), Enum.at(splited_cmd, 2)}})
+          if Enum.count(splitted_cmd) >= 3 do
+            Agent.update(:username, fn _ -> Enum.at(splitted_cmd, 1) end)
+            send_req_user(dir_node, {:login, {Enum.at(splitted_cmd, 1), Enum.at(splitted_cmd, 2)}})
           else
             Prompt.display("incorrect format : login [username] [password]")
             prompt_1(dir_node)
@@ -54,13 +93,15 @@ defmodule Ui do
     end
   end
 
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   defp prompt_2(dir_node) do
     cmd = Prompt.text(">")
-    splited_cmd = String.split(cmd, " ", trim: true)
-    if Enum.count(splited_cmd) == 0 do
+    splitted_cmd = String.split(cmd, " ", trim: true)
+    if Enum.count(splitted_cmd) == 0 do
       prompt_2(dir_node)
     else
-      [action | _] = splited_cmd
+      [action | _] = splitted_cmd
 
       user = Agent.get(:username, fn state -> state end, :infinity)
 
@@ -68,19 +109,22 @@ defmodule Ui do
         "list_users" ->
           send_req_user(dir_node, {:list_users, nil})
         "send" ->
-          if Enum.count(splited_cmd) >= 3 do
-            send_req_mess(dir_node, {:send_message, {{user, Enum.at(splited_cmd, 1)}, Enum.at(splited_cmd, 2)}})
+          if Enum.count(splitted_cmd) >= 3 do
+            message = get_message_to_send(splitted_cmd)
+            send_req_mess(dir_node, {:send_message, {{user, Enum.at(splitted_cmd, 1)}, message}})
           else
             Prompt.display("incorrect format : send [recipient] [message]")
             prompt_2(dir_node)
           end
-        "unseen_mess" ->
+        "new_messages" ->
           send_req_mess(dir_node, {:read_unseen, user})
-        "all_mess" ->
+        "all_messages" ->
           send_req_mess(dir_node, {:read_all, user})
-        "del_read_mess" ->
+        "delete_read_messages" ->
           send_req_mess(dir_node, {:delete_seen, user})
-        "logout" -> :ok
+        "logout" ->
+          Process.exit(self(), :normal)
+        "help" -> print_help(dir_node, 2)
         _ ->
           Prompt.display("invalid command")
           prompt_2(dir_node)
@@ -88,62 +132,44 @@ defmodule Ui do
     end
   end
 
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   def receive_response(dir_node) do
     receive do
       {:ok, {:register, :registered_succesfully}} ->
-        Prompt.display("user registered succesfully!")
-        prompt_2(dir_node)
-      {:error, :user_already_registered} ->
-        Prompt.display("username already taken :")
-        prompt_1(dir_node)
-      {:error, :wrong_password} ->
-        Prompt.display("unsuccessful login")
-        prompt_1(dir_node)
-      {:error, :user_does_not_exist} ->
-        Prompt.display("unsuccessful login")
-        prompt_1(dir_node)
+        display_aux(dir_node, "registered succesfully! :)", 2)
       {:ok, {:login, :correct_password}} ->
-        Prompt.display("welcome!")
-        prompt_2(dir_node)
+        display_aux(dir_node, "welcome!", 2)
+      {:error, :user_already_registered} ->
+        display_aux(dir_node, "username already taken :(", 1)
+      {:error, :wrong_password} ->
+        display_aux(dir_node, "unsuccessful login", 1)
+      {:error, :user_does_not_exist} ->
+        display_aux(dir_node, "unsuccessful login", 1)
       {:ok, {:list_users, name_list}} ->
-        Prompt.display(Enum.join(name_list, "\n"))
-        prompt_2(dir_node)
+        display_aux(dir_node, Enum.join(name_list, "\n"), 2)
       {:error, {:send_message, :user_does_not_exist}} ->
-        Prompt.display("recipient does not exist")
+        display_aux(dir_node, "recipient does not exist", 2)
         prompt_2(dir_node)
       {:ok, {:send_message, :messsage_sent_succesfully}} ->
-        Prompt.display("messsage sent succesfully")
-        prompt_2(dir_node)
-      {:error, {:read_unseen, :user_does_not_exist}} ->
-        Prompt.display("There are not unread messages")
+        display_aux(dir_node, "messsage sent succesfully", 2)
         prompt_2(dir_node)
       {:ok, {:read_unseen, unseen_m_l}} ->
-        Prompt.table([["From:", "Message:"] | Enum.map(unseen_m_l, fn e -> Enum.to_list(e) end)], header: true)
+        Process.sleep(500)
+        Prompt.table([["From:", "Message:"] | Enum.map(unseen_m_l, fn e -> tuple_to_list(e) end)], header: true)
         prompt_2(dir_node)
       {:error, {:read_all, :user_does_not_exist}} ->
-        Prompt.display("There are not any messages")
+        display_aux(dir_node, "There are not any messages", 2)
         prompt_2(dir_node)
       {:ok, {:read_all, all_m_l}} ->
-        Prompt.table([["From:", "Message:"] | Enum.map(all_m_l, fn e -> Enum.to_list(e) end)], header: true)
+        Process.sleep(500)
+        Prompt.table([["From:", "Message:"] | Enum.map(all_m_l, fn e -> tuple_to_list(e) end)], header: true)
         prompt_2(dir_node)
       {:ok, {:delete_seen, :deleted_succesfully}} ->
-        Prompt.display("Readed messages deleted successfully")
-        prompt_2(dir_node)
+        display_aux(dir_node, "Readed messages deleted successfully", 2)
       {:error, _} ->
-        Prompt.display("conextion error : try again later")
+        display_aux(dir_node, "conextion error : try again later", 2)
     end
   end
 
-  def print_help_aux() do
-    Prompt.display("register [username] [password]\nlogin [usernnme] [password]\nlogout\nlist_users\nsend [username] [message]\nnew_messages\nall_messages\ndelete_read_messages\n")
-  end
-
-  def print_help(dir_node, type) do
-    print_help_aux()
-    case type do
-      1 -> prompt_1(dir_node)
-      2 -> prompt_2(dir_node)
-    end
-  end
 end
