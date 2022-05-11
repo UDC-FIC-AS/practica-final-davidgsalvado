daed7850b09283553f1e2dac2ebecac8bb56fe51
David Gayoso <david.gsalvado@udc.es>
2022-05-11 20:07:08 +0200

    fix config

--- lib/emailservice/config.ex
+++ lib/emailservice/config.ex
@@ -10,54 +10,30 @@ defmodule MXConfig do
 
   def init_sv_user do
     UserService.init_user_service
-<<<<<<< HEAD
     UserService.add_user_db(:"u_db@***")
     UserService.add_message_db(:"m_db@***")
-=======
-    UserService.add_user_db(:"u_db@***")
-    UserService.add_message_db(:"m_db@***")
->>>>>>> adolfo&sebas_2
   end
 
   def init_sv_message do
     MessageService.init_message_service
-<<<<<<< HEAD
     MessageService.add_db(:"m_db@***")
-=======
-    MessageService.add_db(:"m_db@***")
->>>>>>> adolfo&sebas_2
   end
 
   def init_lb_users do
     LoadBalancer.init(:user_lbs)
-<<<<<<< HEAD
     LoadBalancer.add_service(:user_lbs, :"s_u1@***")
-=======
-    LoadBalancer.add_service(:user_lbs, :"s_u1@***")
->>>>>>> adolfo&sebas_2
   end
 
   def init_lb_message do
     LoadBalancer.init(:message_lbs)
-<<<<<<< HEAD
     LoadBalancer.add_service(:message_lbs, :"s_m1@***")
     LoadBalancer.add_service(:message_lbs, :"s_m2@***")
-=======
-    LoadBalancer.add_service(:message_lbs, :"s_m1@***")
-    LoadBalancer.add_service(:message_lbs, :"s_m2@***")
->>>>>>> adolfo&sebas_2
   end
 
   def init_dir do
     Directory.init()
-<<<<<<< HEAD
     Directory.add(:user_lbs, :"lb_u1@***")
     Directory.add(:message_lbs, :"lb_m1@***")
     Directory.add(:message_lbs, :"lb_m2@***")
-=======
-    Directory.add(:user_lbs, :"lb_u1@***")
-    Directory.add(:message_lbs, :"lb_m1@***")
-    Directory.add(:message_lbs, :"lb_m2@***")
->>>>>>> adolfo&sebas_2
   end
 end
