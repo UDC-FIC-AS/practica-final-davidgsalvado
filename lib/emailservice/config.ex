1e7fbe99495287b6a7c4b85b0f51473d53d8e81e
David Gayoso <david.gsalvado@udc.es>
2022-05-12 19:01:37 +0200

    actualización

--- lib/emailservice/config.ex
+++ lib/emailservice/config.ex
@@ -42,50 +42,6 @@ defmodule MXConfig do
     Node.spawn(m_db_node, fn -> ServerDb.init_db(:message_db) end)
   end
 
-<<<<<<< HEAD
-defp init_sv_user(s_u_node, u_db_node, m_db_node) do
-  Node.spawn(s_u_node,
-  fn ->
-    UserService.init_user_service
-    UserService.add_user_db(u_db_node)
-    UserService.add_message_db(m_db_node)
-  end)
-end
-
-defp init_sv_message(s_m_node, m_db_node) do
-  Node.spawn(s_m_node,
-  fn ->
-    MessageService.init_message_service
-    MessageService.add_db(m_db_node)
-  end)
-end
-
-defp init_lb_users(lb_u_node, s_u_node) do
-  Node.spawn(lb_u_node,
-  fn ->
-    LoadBalancer.init(:user_lbs)
-    LoadBalancer.add_service(:user_lbs, s_u_node)
-  end)
-end
-
-defp init_lb_message(lb_m_node, s_m1_node, s_m2_node) do
-  Node.spawn(lb_m_node,
-  fn ->
-    LoadBalancer.init(:message_lbs)
-    LoadBalancer.add_service(:message_lbs, s_m1_node)
-    LoadBalancer.add_service(:message_lbs, s_m2_node)
-  end)
-end
-
-defp init_dir(dir_node, lb_u1_node, lb_m1_node, lb_m2_node) do
-  Node.spawn(dir_node,
-  fn ->
-    Directory.init()
-    Directory.add(:user_lbs, lb_u1_node)
-    Directory.add(:message_lbs, lb_m1_node)
-    Directory.add(:message_lbs, lb_m2_node)
-  end)
-=======
   defp init_sv_user(s_u_node, u_db_node, m_db_node) do
     Node.spawn(s_u_node,
     fn ->
@@ -128,6 +84,5 @@ defp init_dir(dir_node, lb_u1_node, lb_m1_node, lb_m2_node) do
       Directory.add(:message_lbs, lb_m1_node)
       Directory.add(:message_lbs, lb_m2_node)
     end)
->>>>>>> adolfo&sebas_2
   end
 end
